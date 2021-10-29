4b01535f575aea64cf3ba9288ea0aa50678a4c5d
ameys <91192362+Xmell91@users.noreply.github.com>
2021-10-29 19:56:53 +0700

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -2,20 +2,17 @@ let limit = 30
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
   if (!args || !args[0]) throw 'Uhm... urlnya mana?'
-  let chat = global.db.data.chats[m.chat]
+  let chat = global.DATABASE.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 *Title:* ${title}
 *Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
+${isLimit ? `*Pakai Link:* ${await shorten(dl_link)} `: ''}
 `.trim(), m)
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-`.trim(), m, null, {
-  asDocument: chat.useDocument
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', ``.trim(), m, null, {
+  asDocument: false
 })
 }
 handler.help = ['mp3','a'].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
@@ -36,7 +33,6 @@ handler.limit = true
 
 module.exports = handler
 
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
+async function shorten(url){
+return url ? (await require('axios').get(`https://is.gd/create.php?format=simple&url=${url}`)).data : ''
 }
