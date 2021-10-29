8e584ed6ab3a0191d138e384da6bc13110e1cf7f
ameys <91192362+Xmell91@users.noreply.github.com>
2021-10-29 19:59:43 +0700

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -1,23 +1,54 @@
 let yts = require('yt-search')
-let handler = async (m, { text }) => {
+
+let handler = async (m, { text, conn }) => {
   if (!text) throw 'Cari apa?'
   let results = await yts(text)
+  let anjing = []
   let teks = results.all.map(v => {
     switch (v.type) {
-      case 'video': return `
-*${v.title}* (${v.url})
+      case 'video': 
+      anjing.push({
+      "title": `${v.title}`,
+      "rows": [
+        {
+          "title": `${v.title}`,
+          "description": `*${v.title}* (${v.url})
 Duration: ${v.timestamp}
 Uploaded ${v.ago}
 ${v.views} views
-      `.trim()
-      case 'channel': return `
+      `.trim(),
+          "rowId": `/ytplay ${v.url}`
+        }
+      ]
+    })
+    break
+      case 'channel':
+      anjing.push({
+      "title": `${v.title}`,
+      "rows": [
+        {
+          "title": `${v.name}`,
+          "description":  `
 *${v.name}* (${v.url})
 _${v.subCountLabel} (${v.subCount}) Subscriber_
 ${v.videoCount} video
-`.trim()
+`.trim(),
+          "rowId": `/ytstalk ${v.name}`
+        }
+      ]
+    })
+    break
     }
   }).filter(v => v).join('\n========================\n')
-  m.reply(teks)
+  hem = {
+  "title": "YT-SEARCH",
+  "description": "Silahkan Gan Dipilih              ",
+  "buttonText": "PENCET",
+  "listType": "SINGLE_SELECT",
+  "sections": anjing
+  }
+  hilih = await conn.prepareMessageFromContent(m.chat, { listMessage: hem }, {quoted: m, sendEphemeral: true, thumbnail: global.thumbnail ? global.thumbnail : Buffer.alloc(0)})
+  conn.relayWAMessage(hilih)
 }
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
 handler.tags = ['tools']
